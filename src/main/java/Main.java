34a49e75ecdc8166e344df577345e0195c4fa608
BuildpackTester <jpkutner@gmail.com>
2017-08-30 09:11:40 -0500

    Use a heap memory leak instead of non-heap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.*;
 
 public class Main extends HttpServlet {
 
-  List<ByteBuffer> buffers = Collections.synchronizedList(new ArrayList<ByteBuffer>());
+  static final Integer METABYTE = 1000000;
+  static final Integer ARRAY_SIZE = METABYTE * 50;
+
+  List<byte[]> bytes = Collections.synchronizedList(new ArrayList<byte[]>());
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    // leak some direct memory buffers
-    buffers.add(ByteBuffer.allocateDirect(40000000));
-    resp.getWriter().print("Direct Memory Buffers: " + buffers.size());
+    byte[] b = new byte[ARRAY_SIZE];
+    new Random().nextBytes(b);
+    bytes.add(b);
+    resp.getWriter().print(bytes.size() * (ARRAY_SIZE / METABYTE) + " Megabytes leaked");
   }
 
   public static void main(String[] args) throws Exception{
